e7481f7169e84b4f9a8342549893998d23d9d102
Rifqi Muzhaffar <rifqi.city@gmail.com>
2024-12-09 00:48:42 +0700

    feat: Add detail page, CardCarousel template, and dummy data for cards

---
+++ src/dummydata/card.js
@@ -0,0 +1,44 @@
+const cards = [
+  {
+    title: "Kursus A",
+    description: "Belajar React JS",
+    content: "Konten 1.",
+  },
+  {
+    title: "Kursus B",
+    description: "Belajar Node.js",
+    content: "Konten 2.",
+  },
+  {
+    title: "Kursus C",
+    description: "Belajar Next.js",
+    content: "Konten 3.",
+  },
+  {
+    title: "Kursus D",
+    description: "Belajar Tailwind CSS",
+    content: "Konten 4.",
+  },
+  {
+    title: "Kursus E",
+    description: "Belajar Prisma ORM",
+    content: "Konten 5.",
+  },
+  {
+    title: "Kursus F",
+    description: "Belajar Sequalize",
+    content: "Konten 6.",
+  },
+  {
+    title: "Kursus G",
+    description: "Belajar Py",
+    content: "Konten 6.",
+  },
+  {
+    title: "Kursus H",
+    description: "Belajar JS",
+    content: "Konten 7.",
+  },
+];
+
+export default cards;
