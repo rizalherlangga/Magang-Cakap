c06d56d6e8fbb3567823f15e3ac8335d6b44432d
Rifqi Muzhaffar <rifqi.city@gmail.com>
2024-12-09 23:59:21 +0700

    refactor: split about course detail section into fragments and templates

--- src/app/courses/[id]/page.jsx
+++ src/app/courses/[id]/page.jsx
@@ -2,6 +2,7 @@
 
 import BannerPage from "@/components/fragments/banner-page";
 import ListDescription from "@/components/fragments/list-description";
+import AboutCourseTemplate from "@/components/template/about-course-template";
 import CourseCarousel from "@/components/template/card-carousel";
 import cards from "@/dummydata/card";
 import { useState } from "react";
@@ -25,8 +26,6 @@ export default function DetailList() {
     console.log("Card clicked:", card);
   };
 
-  const cardsPerView = 4;
-
   return (
     <div className="text-white">
       <BannerPage>
@@ -43,50 +42,7 @@ export default function DetailList() {
           </div>
         </div>
       </BannerPage>
-      <section className="flex">
-        <div className="w-9/12 border border-white py-4 pr-20 text-black">
-          <div>
-            <div className="flex mb-6">
-              <div>
-                <img src="https://via.placeholder.com/50x50" alt="" />
-              </div>
-              <div>
-                <h1>Institusi</h1>
-                <h2>Cakap</h2>
-              </div>
-            </div>
-            <div>
-              <p className="text-sm">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Exercitationem, blanditiis vero? Autem quibusdam error eveniet
-                modi voluptate ea, quos odit minima? Quis numquam eum modi earum
-                illo suscipit vitae, odit delectus quia id, recusandae eius
-                quod, laboriosam iusto perspiciatis culpa.
-              </p>
-            </div>
-          </div>
-          {Array(5)
-            .fill(0)
-            .map((_, index) => (
-              <ListDescription key={index} />
-            ))}
-        </div>
-        <div className="bg-black w-3/12 border border-white p-4">
-          <div className="flex justify-between">
-            <p>10%</p>
-            <p>Rp 100.000</p>
-            <p>Rp 90.000</p>
-          </div>
-          <div>
-            <button className="w-full border border-white rounded-md">
-              BELI KURSUS
-            </button>
-            <button className="w-full border border-white rounded-md">
-              TUKAR KODE PELAJAR
-            </button>
-          </div>
-        </div>
-      </section>
+      <AboutCourseTemplate />
       <CourseCarousel
         title="Kursus Terkait"
         cards={cards}
