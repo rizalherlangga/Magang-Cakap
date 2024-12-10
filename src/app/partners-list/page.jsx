359d130f0fe4c574901bb593bca550cff8f665f2
Rifqi Muzhaffar <rifqi.city@gmail.com>
2024-12-11 00:49:42 +0700

    feat: create live-bahasa template, add fetchDetail API

--- src/app/partners-list/page.jsx
+++ src/app/partners-list/page.jsx
@@ -1,11 +1,9 @@
-import LiveBahasa from "@/components/template/live-bahasa";
 import PartnersListTemplate from "@/components/template/parners-list-template";
 
 export default function PartnersList() {
   return (
     <div>
       <PartnersListTemplate />
-      <LiveBahasa />
     </div>
   );
 }
